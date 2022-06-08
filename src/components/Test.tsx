34eae6c927dcc16dcaffadf7c0c41c23df660205
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2022-06-08 12:55:44 +0200

    Added working piece of code

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,10 +1,60 @@
 import { observer } from "mobx-react";
 import React from "react";
+import { makeObservable, observable, computed, autorun, action } from "mobx";
 import mainStore from "../stores/MainStore";
 
+class OrderLine {
+  price = 0;
+  amount = 1;
+
+  constructor() {
+    makeObservable(this, {
+      price: observable,
+      amount: observable,
+      total: computed
+    });
+    console.log("From constructor .....................");
+  }
+
+  setPrice = action(value => (this.price = value));
+
+  get total() {
+    console.log("Computing...");
+    return this.price * this.amount;
+  }
+}
+
 class TestComponent extends React.Component {
+  componentDidMount() {
+    setTimeout(() => {
+      const order = new OrderLine();
+
+      autorun(() => {
+        console.log("Total: " + order.total);
+      });
+      // Computing...
+      // Total: 0
+
+      console.log(order.total);
+      // (No recomputing!)
+      // 0
+
+      order.setPrice(5);
+      // Computing...
+      // (No autorun)
+
+      order.setPrice(2);
+      // Computing...
+      // Total: 10
+
+      //stop();
+
+      order.setPrice(3);
+    }, 2000);
+  }
+
   render() {
-    return <div className="statistics">TEST component</div>;
+    return <div className="statistics">TEST component: {mainStore.observable1}</div>;
   }
 }
 
--- src/stores/MainStore.tsx
+++ src/stores/MainStore.tsx
@@ -1,15 +1,28 @@
 import { observable, action, configure, makeObservable, computed } from "mobx";
 
 configure({
-  enforceActions: "always",
-  computedRequiresReaction: true,
-  reactionRequiresObservable: true,
-  observableRequiresReaction: true
+  enforceActions: "always"
 });
 
-export class MainStore {
+class MainStore {
+  observable1 = "";
+  observable2 = "";
+
   constructor() {
-    makeObservable(this, {});
+    makeObservable(this, {
+      observable1: observable,
+      observable2: observable,
+      setObservable1: action
+    });
+  }
+
+  setObservable1 = action((value: string) => {
+    console.log("Observable changed to:", value);
+    this.observable1 = value;
+  });
+
+  autorun() {
+    console.log("Autorun executed");
   }
 }
 
