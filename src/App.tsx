074fbbcb730222e23d95e6fb4ea0064472f63d0c
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2022-06-08 12:13:05 +0200

    Added store and one component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./App.css";
-
+import TestComponent from "./components/Test";
 class App extends React.Component {
   render() {
-    return <React.Fragment>THIS IS REACT TEST APP</React.Fragment>;
+    return (
+      <React.Fragment>
+        <TestComponent />
+      </React.Fragment>
+    );
   }
 }
 
