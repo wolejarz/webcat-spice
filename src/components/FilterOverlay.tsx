05476bfc49c318bb7d10b19e508bf6b00059142d
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2022-06-14 11:29:14 +0200

    Defined Filter model

--- src/components/FilterOverlay.tsx
+++ src/components/FilterOverlay.tsx
@@ -1,14 +1,41 @@
 import { TextField } from "@mui/material";
+import { Console } from "console";
 import { observer } from "mobx-react";
 import React, { Fragment } from "react";
 import mainStore from "../stores/MainStore";
 
 class FilterOverlay extends React.Component {
+  handleMinValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("minvalue changed before:", mainStore.columnDefinitions[mainStore.filterColumnIndex].minValue);
+    mainStore.columnDefinitions[mainStore.filterColumnIndex].minValue = event.target.value;
+    console.log("minvalue changed afte:", mainStore.columnDefinitions[mainStore.filterColumnIndex].minValue);
+  };
+
+  handleMaxValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    mainStore.columnDefinitions[mainStore.filterColumnIndex].maxValue = event.target.value;
+  };
+
   render() {
+    const index = mainStore.filterColumnIndex;
+    console.log("index from render", index);
     return (
       <Fragment>
-        <TextField id="min-value" label="MinValue" variant="outlined" />
-        <TextField id="max-value" label="MaxValue" variant="outlined" />
+        <TextField
+          id="min-value"
+          value={mainStore.columnDefinitions[index].minValue}
+          onChange={this.handleMinValueChange}
+          label="MinValue"
+          variant="outlined"
+          size="small"
+        />
+        <TextField
+          id="max-value"
+          value={mainStore.columnDefinitions[index].maxValue}
+          onChange={this.handleMaxValueChange}
+          label="MaxValue"
+          variant="outlined"
+          size="small"
+        />
       </Fragment>
     );
   }
